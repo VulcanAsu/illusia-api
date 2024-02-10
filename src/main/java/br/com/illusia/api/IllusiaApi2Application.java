178f02a7b24309ab09e4b6bda04191312a068a3c
NekoYasha <autornekoyasha@gmail.com>
2024-02-10 19:38:17 -0300

    first commit

---
+++ src/main/java/br/com/illusia/api/IllusiaApi2Application.java
@@ -0,0 +1,13 @@
+package br.com.illusia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllusiaApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllusiaApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/illusia/api/IllusiaApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.illusia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllusiaApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
