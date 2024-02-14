4c5a69cbfbe8220ee2dbc708f2d31cd755d32349
NekoYasha <autornekoyasha@gmail.com>
2024-02-14 13:47:55 -0300

    📜 feat: Adiciona entidade de persistência com UUID

--- src/main/java/br/com/illusia/api/infrastructure/model/PersistenceEntity.java
+++ src/main/java/br/com/illusia/api/infrastructure/model/PersistenceEntity.java
@@ -1,2 +1,30 @@
-package br.com.illusia.api.infrastructure.model;public class PersistenceEntity {
+// ✦•······················• PACKAGE - START •······················•✦ //
+package br.com.illusia.api.infrastructure.model;
+// ✦•······················• PACKAGE - END •······················•✦ //
+
+// ✦•······················• IMPORTS - START •······················•✦ //
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+import java.util.UUID;
+// ✦•······················• IMPORTS - END •······················•✦ //
+
+/**
+ * @author Giovane Neves
+ * @since v1
+ */
+@Data
+@MappedSuperclass
+public class PersistenceEntity {
+
+    // ✦•······················• ATTRIBUTES - START •······················•✦ //
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    // ✦•······················• ATTRIBUTES - END •······················•✦ //
+
 }
