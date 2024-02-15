f27340978cde3b76eab703db74193ebb22539305
NekoYasha <autornekoyasha@gmail.com>
2024-02-14 21:39:44 -0300

    Cria repositório para entitade 'User'

--- src/main/java/br/com/illusia/api/entity/user/repository/UserRepository.java
+++ src/main/java/br/com/illusia/api/entity/user/repository/UserRepository.java
@@ -1,2 +1,17 @@
-package br.com.illusia.api.entity.user.repository;public interface UserRepository {
+// ✦•······················• PACKAGE - START •······················•✦ //
+package br.com.illusia.api.entity.user.repository;
+// ✦•······················• PACKAGE - END •······················•✦ //
+
+// ✦•······················• IMPORTS - START •······················•✦ //
+import br.com.illusia.api.entity.user.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+// ✦•······················• IMPORTS - END •······················•✦ //
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
 }
