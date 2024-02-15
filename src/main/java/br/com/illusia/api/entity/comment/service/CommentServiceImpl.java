ec78e56240c522a279fd0c26ca4bc3c12779c2c1
Andesson Reis <andessonreys@gmail.com>
2024-02-15 12:10:17 -0300

    ✨ feat(comment): Add CommentService implementation
    Adds the implementation of the CommentService interface.
    The CommentServiceImpl class provides methods for saving comments to the database.

---
+++ src/main/java/br/com/illusia/api/entity/comment/service/CommentServiceImpl.java
@@ -0,0 +1,21 @@
+// ✦•······················• PACKAGE - START •······················•✦ //
+package br.com.illusia.api.entity.comment.service;
+// ✦•······················• PACKAGE - END •······················•✦ //
+
+// ✦•······················• IMPORTS - START •······················•✦ //
+import br.com.illusia.api.entity.comment.model.Comment;
+
+import org.springframework.stereotype.Service;
+// ✦•······················• IMPORTS - END •······················•✦ //
+
+@Service
+public class CommentServiceImpl implements ICommentService {
+
+    @Override
+    public void saveComment(Comment comment) {
+        
+        // TODO: Criar regras de negócio para salvar comentario.
+
+    }
+
+}
