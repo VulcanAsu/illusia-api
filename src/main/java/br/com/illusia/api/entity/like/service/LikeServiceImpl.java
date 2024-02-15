dd95b1c78e624684db77776312c1b399b45106ec
Andesson Reis <andessonreys@gmail.com>
2024-02-15 18:12:46 -0300

    ✨ feat(like): Add LikeServiceImpl implementation
    Adds the implementation of the ILikeService interface.
    The LikeServiceImpl class provides methods for handling like-related operations.

---
+++ src/main/java/br/com/illusia/api/entity/like/service/LikeServiceImpl.java
@@ -0,0 +1,11 @@
+// ✦•······················• PACKAGE - START •······················•✦ //
+package br.com.illusia.api.entity.like.service;
+// ✦•······················• PACKAGE - END •······················•✦ //
+
+// ✦•······················• IMPORTS - START •······················•✦ //
+import org.springframework.stereotype.Service;
+// ✦•······················• IMPORTS - END •······················•✦ //
+@Service
+public class LikeServiceImpl implements ILikeService{
+        
+}
