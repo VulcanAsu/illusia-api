d0c2e54b87dfcf5ec3d71a55ac487f62e5b93b82
Andesson Reis <andessonreys@gmail.com>
2024-02-15 11:59:13 -0300

    ✨ feat(comment): Add Comment entity representing user comments
    Adds the Comment entity, which represents a comment made by a user. It includes attributes for "content", "date", "user", and "replies".
    
    -The "content" attribute represents the text content of the comment.
    -The "date" attribute stores the date and time when the comment was created.
    -The "user" attribute establishes a many-to-one relationship with the User entity, representing the user who made the comment.
    -The "replies" attribute establishes a one-to-many relationship with other Comment entities, representing replies to this comment.

---
+++ src/main/java/br/com/illusia/api/entity/comment/model/Comment.java
@@ -0,0 +1,58 @@
+// ✦•······················• PACKAGE - START •······················•✦ //
+package br.com.illusia.api.entity.comment.model;
+// ✦•······················• PACKAGE - END •······················•✦ //
+
+// ✦•······················• IMPORTS - START •······················•✦ //
+import br.com.illusia.api.infrastructure.model.PersistenceEntity;
+import br.com.illusia.api.entity.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+// ✦•······················• IMPORTS - END •······················•✦ //
+
+/**
+ * Represents a comment made by a user.
+ * 
+ * @author Andesson Reis
+ * @since v1
+ */
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends PersistenceEntity {
+
+    // ✦•······················• ATTRIBUTES - START •······················•✦ //
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "comment_replies",
+            joinColumns = @JoinColumn(name = "comment_id"),
+            inverseJoinColumns = @JoinColumn(name = "reply_id"))
+    private List<Comment> replies;
+
+    // ✦•······················• ATTRIBUTES - END •······················•✦ //
+}
